829d85f83f44daa304d4e049706d1ba903caf948
Ibn-Ahmed <ahmedibn19@yahoo.com>
2022-09-05 07:51:23 +0000

    New Year

---
+++ counter.js
@@ -0,0 +1,30 @@
+const newYear = "1 Jan 2023";
+let secondsEl = document.getElementById("seconds");
+let hoursEl = document.getElementById("hours");
+let minutesEl = document.getElementById("minutes");
+let daysEl = document.getElementById("days");
+
+function countdown() {
+  const newYearsDate = new Date(newYear);
+  const currentDate = new Date();
+  let totalSeconds = (newYearsDate - currentDate) / 1000;
+  let seconds = Math.floor(totalSeconds) % 60;
+  let mins = Math.floor(totalSeconds / 60) % 60;
+  let hours = Math.floor(totalSeconds / 3600) % 24;
+  let days = Math.floor(totalSeconds / 3600 / 24);
+
+  secondsEl.textContent = timeFormat(seconds);
+  hoursEl.textContent = timeFormat(hours);
+  minutesEl.textContent = timeFormat(mins);
+  daysEl.textContent = timeFormat(days);
+}
+
+function timeFormat(time) {
+  if (time < 10) {
+    return `0${time}`;
+  } else {
+    return time;
+  }
+}
+countdown();
+setInterval(countdown, 1000);
